1dda915bcaa9466a53ae85fbeae3855bc95e2a5c
98tttm <tranthanhthinh880@gmail.com>
2025-11-08 01:11:25 +0700

    Update production API URLs

--- my_client/src/environments/environment.prod.ts
+++ my_client/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 // ⚠️ Sau khi deploy backend lên Railway, thay YOUR_BACKEND_URL bằng URL thật
 export const environment = {
   production: true,
-  apiUrl: 'YOUR_BACKEND_URL'  // Ví dụ: https://medicare-backend-production.up.railway.app
+  apiUrl: 'https://medicare-production-70ae.up.railway.app'  // Ví dụ: https://medicare-backend-production.up.railway.app
 };
 
